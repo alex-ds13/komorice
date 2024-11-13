8e14512eb72b6a45bd448bae500da44b155f2efb
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2024-11-13 10:42:01 +0000

    feat: add more workspace configs

--- src/screen/workspace.rs
+++ src/screen/workspace.rs
@@ -1,6 +1,6 @@
 use iced::widget::column;
 use iced::{widget::text, Element};
-use komorebi::WorkspaceConfig;
+use komorebi::{WindowContainerBehaviour, WorkspaceConfig};
 use komorebi_client::DefaultLayout;
 
 use crate::utils::DisplayOptionCustom as DisplayOption;
@@ -13,8 +13,14 @@ lazy_static! {
         DisplayOption(None, "[None] (Floating)"),
         DisplayOption(Some(DefaultLayout::BSP), "[None] (Floating)"),
         DisplayOption(Some(DefaultLayout::VerticalStack), "[None] (Floating)"),
-        DisplayOption(Some(DefaultLayout::RightMainVerticalStack), "[None] (Floating)"),
-        DisplayOption(Some(DefaultLayout::UltrawideVerticalStack), "[None] (Floating)"),
+        DisplayOption(
+            Some(DefaultLayout::RightMainVerticalStack),
+            "[None] (Floating)"
+        ),
+        DisplayOption(
+            Some(DefaultLayout::UltrawideVerticalStack),
+            "[None] (Floating)"
+        ),
         DisplayOption(Some(DefaultLayout::HorizontalStack), "[None] (Floating)"),
         DisplayOption(Some(DefaultLayout::Rows), "[None] (Floating)"),
         DisplayOption(Some(DefaultLayout::Columns), "[None] (Floating)"),
@@ -91,6 +97,40 @@ impl WorkspaceScreen for WorkspaceConfig {
             Some(DisplayOption(self.layout, "[None] (Floating)")),
             |s| Message::ConfigChange(ConfigChange::Layout(s.0)),
         );
+        let apply_window_based_offset = opt_helpers::toggle(
+            "Apply Window Based Work Area Offset:",
+            Some("Apply this monitor's window-based work area offset (default: true)"),
+            self.apply_window_based_work_area_offset.unwrap_or(true),
+            |v| Message::ConfigChange(ConfigChange::ApplyWindowBasedWorkAreaOffset(v)),
+        );
+        let container_padding = opt_helpers::number(
+            "Container Padding:",
+            Some("Container padding (default: global)"),
+            self.container_padding.unwrap_or_default(),
+            |v| Message::ConfigChange(ConfigChange::ContainerPadding(v)),
+        );
+        let float_override = opt_helpers::toggle(
+            "Float Override:",
+            Some("Enable or disable float override, which makes it so every new window opens in floating mode (default: false)"),
+            self.float_override.unwrap_or_default(),
+            |v| Message::ConfigChange(ConfigChange::FloatOverride(v)),
+        );
+        let window_container_behaviour = opt_helpers::choose(
+            "Window Container Behaviour:",
+            Some("Determine what happens when a new window is opened (default: Create)"),
+            [
+                WindowContainerBehaviour::Create,
+                WindowContainerBehaviour::Append,
+            ],
+            self.window_container_behaviour,
+            |v| Message::ConfigChange(ConfigChange::WindowContainerBehaviour(v)),
+        );
+        let workspace_padding = opt_helpers::number(
+            "Workspace Padding:",
+            Some("Workspace padding (default: global)"),
+            self.workspace_padding.unwrap_or_default(),
+            |v| Message::ConfigChange(ConfigChange::WorkspacePadding(v)),
+        );
         // opt_helpers::expandable(
         //     title,
         //     None,
@@ -98,6 +138,17 @@ impl WorkspaceScreen for WorkspaceConfig {
         //     expanded,
         //     Message::ToggleExpanded(idx),
         // )
-        column![title, name, layout].into()
+        column![
+            title,
+            name,
+            layout,
+            apply_window_based_offset,
+            container_padding,
+            float_override,
+            window_container_behaviour,
+            workspace_padding
+        ]
+        .spacing(10)
+        .into()
     }
 }
