41b82d8f60333b858a895c4d1b7689ed4d36d1ca
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2024-11-14 00:41:31 +0000

    fix: remove extra padding from monitor on monitors

--- src/screen/monitor.rs
+++ src/screen/monitor.rs
@@ -1,9 +1,10 @@
+use super::workspace::{self, WorkspaceScreen};
+
+use crate::{widget::opt_helpers, BOLD_FONT};
+
 use std::collections::HashMap;
 
-use super::workspace::{self, WorkspaceScreen};
-use crate::widget::opt_helpers;
-
-use iced::{widget::text, Element, Task};
+use iced::{padding, widget::{column, horizontal_rule, text, Column}, Element, Task};
 use komorebi::MonitorConfig;
 
 #[derive(Clone, Debug)]
@@ -194,9 +195,8 @@ impl Monitor {
     }
 
     pub fn view(&self) -> Element<Message> {
-        opt_helpers::section_view(
-            text!("Monitor [{}]:", self.index),
-            [
+        let section_title = text!("Monitor [{}]:", self.index).size(18.0);
+        let contents = [
             opt_helpers::expandable(
                 "Window Based Work Area Offset",
                 Some("Window based work area offset (default: None)"),
@@ -230,76 +230,84 @@ impl Monitor {
                 self.window_based_work_area_offset_hovered,
                 Message::ToggleWindowBasedWorkAreaOffsetExpand,
                 Message::ToggleWindowBasedWorkAreaOffsetHover,
-                ),
-                opt_helpers::number(
-                    "Window Based Work Area Offset Limit",
-                    Some("Open window limit after which the window based work area offset will no longer be applied (default: 1)"),
-                    self.config.window_based_work_area_offset_limit.unwrap_or(1).try_into().unwrap_or_default(),
-                    move |value| {
-                        Message::ConfigChange(
-                            
-                            ConfigChange::WindowBasedWorkAreaOffsetLimit(value),
+            ),
+            opt_helpers::number(
+                "Window Based Work Area Offset Limit",
+                Some("Open window limit after which the window based work area offset will no longer be applied (default: 1)"),
+                self.config.window_based_work_area_offset_limit.unwrap_or(1).try_into().unwrap_or_default(),
+                move |value| {
+                    Message::ConfigChange(
+                        
+                        ConfigChange::WindowBasedWorkAreaOffsetLimit(value),
+                    )
+                },
+            ),
+            opt_helpers::expandable(
+                "Work Area Offset",
+                Some("Monitor-specific work area offset (default: None)"),
+                [
+                    opt_helpers::number(
+                        "left",
+                        None,
+                        self.config.work_area_offset.map_or(0, |r| r.left),
+                        move |value| Message::ConfigChange(ConfigChange::WorkAreaOffsetLeft(value)),
+                    ),
+                    opt_helpers::number(
+                        "top",
+                        None,
+                        self.config.work_area_offset.map_or(0, |r| r.top),
+                        move |value| Message::ConfigChange(ConfigChange::WorkAreaOffsetTop(value)),
+                    ),
+                    opt_helpers::number(
+                        "bottom",
+                        None,
+                        self.config.work_area_offset.map_or(0, |r| r.bottom),
+                        move |value| Message::ConfigChange(ConfigChange::WorkAreaOffsetBottom(value)),
+                    ),
+                    opt_helpers::number(
+                        "right",
+                        None,
+                        self.config.work_area_offset.map_or(0, |r| r.right),
+                        move |value| Message::ConfigChange(ConfigChange::WorkAreaOffsetRight(value)),
+                    ),
+                ],
+                self.work_area_offset_expanded,
+                self.work_area_offset_hovered,
+                Message::ToggleWorkAreaOffsetExpand,
+                Message::ToggleWorkAreaOffsetHover,
+            ),
+            opt_helpers::expandable(
+                "Workspaces",
+                None,
+                self.config.workspaces
+                    .iter()
+                    .enumerate().
+                    map(|(i, w)| {
+                        let title = text!("Workspace [{}] - \"{}\":", i, w.name).size(20);
+                        opt_helpers::expandable(
+                            title,
+                            None,
+                            [w.view(i, self.expanded_workspaces[&i]).map(move |m| Message::Workspace(i, m))],
+                            self.expanded_workspaces[&i],
+                            false,
+                            Message::ToggleWorkspaceExpanded(i),
+                            |_| Message::Nothing,
                         )
-                    },
-                ),
-                opt_helpers::expandable(
-                    "Work Area Offset",
-                    Some("Monitor-specific work area offset (default: None)"),
-                    [
-                        opt_helpers::number(
-                            "left",
-                            None,
-                            self.config.work_area_offset.map_or(0, |r| r.left),
-                            move |value| Message::ConfigChange(ConfigChange::WorkAreaOffsetLeft(value)),
-                        ),
-                        opt_helpers::number(
-                            "top",
-                            None,
-                            self.config.work_area_offset.map_or(0, |r| r.top),
-                            move |value| Message::ConfigChange(ConfigChange::WorkAreaOffsetTop(value)),
-                        ),
-                        opt_helpers::number(
-                            "bottom",
-                            None,
-                            self.config.work_area_offset.map_or(0, |r| r.bottom),
-                            move |value| Message::ConfigChange(ConfigChange::WorkAreaOffsetBottom(value)),
-                        ),
-                        opt_helpers::number(
-                            "right",
-                            None,
-                            self.config.work_area_offset.map_or(0, |r| r.right),
-                            move |value| Message::ConfigChange(ConfigChange::WorkAreaOffsetRight(value)),
-                        ),
-                    ],
-                    self.work_area_offset_expanded,
-                    self.work_area_offset_hovered,
-                    Message::ToggleWorkAreaOffsetExpand,
-                    Message::ToggleWorkAreaOffsetHover,
-                ),
-                opt_helpers::expandable(
-                    "Workspaces",
-                    None,
-                    self.config.workspaces
-                        .iter()
-                        .enumerate().
-                        map(|(i, w)| {
-                            let title = text!("Workspace [{}] - \"{}\":", i, w.name).size(20);
-                            opt_helpers::expandable(
-                                title,
-                                None,
-                                [w.view(i, self.expanded_workspaces[&i]).map(move |m| Message::Workspace(i, m))],
-                                self.expanded_workspaces[&i],
-                                false,
-                                Message::ToggleWorkspaceExpanded(i),
-                                |_| Message::Nothing,
-                            )
-                        }),
-                    self.show_workspaces,
-                    self.show_workspaces_hovered,
-                    Message::ToggleWorkspacesExpand,
-                    Message::ToggleWorkspacesHover,
-                ),
-            ],
-        )
+                    }),
+                self.show_workspaces,
+                self.show_workspaces_hovered,
+                Message::ToggleWorkspacesExpand,
+                Message::ToggleWorkspacesHover,
+            ),
+        ];
+        column![
+            section_title,
+            horizontal_rule(2.0),
+            Column::with_children(contents)
+                .padding(padding::top(10).bottom(10))
+                .spacing(10),
+        ]
+        .spacing(10)
+        .into()
     }
 }
