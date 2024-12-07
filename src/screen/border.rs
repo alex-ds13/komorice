2ee231eda9525f321b0446eb309ec3c57061e630
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2024-12-07 13:34:27 +0000

    feat(border): finish Border screen implementation

--- src/screen/border.rs
+++ src/screen/border.rs
@@ -1,9 +1,10 @@
-use crate::widget::opt_helpers;
+use crate::widget::opt_helpers::description_text as t;
+use crate::{utils::DisplayOption, widget::opt_helpers};
 
 use iced::{Element, Task};
 use komorebi::{
     border_manager::ZOrder, BorderColours, BorderImplementation, BorderStyle, Colour, Rgb,
-    StackbarLabel, StackbarMode, StaticConfig,
+    StaticConfig,
 };
 
 #[derive(Clone, Debug)]
@@ -13,6 +14,7 @@ pub enum Message {
     ToggleMonoclePicker(bool),
     ToggleUnfocusedPicker(bool),
     ToggleFloatingPicker(bool),
+    ToggleStackPicker(bool),
 }
 
 #[derive(Clone, Debug)]
@@ -22,11 +24,12 @@ pub enum ConfigChange {
     BorderOffset(i32),
     BorderStyle(BorderStyle),
     BorderImplementation(BorderImplementation),
-    BorderZOrder(ZOrder),
+    BorderZOrder(Option<ZOrder>),
     SingleColor(Option<iced::Color>),
     MonocleColor(Option<iced::Color>),
     UnfocusedColor(Option<iced::Color>),
     FloatingColor(Option<iced::Color>),
+    StackColor(Option<iced::Color>),
 }
 
 #[derive(Clone, Debug)]
@@ -40,6 +43,7 @@ pub struct Border {
     pub show_monocle_picker: bool,
     pub show_unfocused_picker: bool,
     pub show_floating_picker: bool,
+    pub show_stack_picker: bool,
 }
 
 #[derive(Debug)]
@@ -55,13 +59,13 @@ pub struct BorderConfig<'a> {
 
 #[derive(Debug)]
 pub struct BorderConfigMut<'a> {
-    pub border: Option<&'a mut bool>,
-    pub border_colours: Option<&'a mut BorderColours>,
-    pub border_implementation: Option<&'a mut BorderImplementation>,
-    pub border_style: Option<&'a mut BorderStyle>,
-    pub border_width: Option<&'a mut i32>,
-    pub border_offset: Option<&'a mut i32>,
-    pub border_z_order: Option<&'a mut ZOrder>,
+    pub border: &'a mut Option<bool>,
+    pub border_colours: &'a mut Option<BorderColours>,
+    pub border_implementation: &'a mut Option<BorderImplementation>,
+    pub border_style: &'a mut Option<BorderStyle>,
+    pub border_width: &'a mut Option<i32>,
+    pub border_offset: &'a mut Option<i32>,
+    pub border_z_order: &'a mut Option<ZOrder>,
 }
 
 // impl Default for BorderConfig {
@@ -91,18 +95,99 @@ impl Border {
         config: &mut StaticConfig,
     ) -> (Action, Task<Message>) {
         match message {
-            Message::ConfigChange(change) => match change {
-                ConfigChange::Border(_) => todo!(),
-                ConfigChange::BorderWidth(_) => todo!(),
-                ConfigChange::BorderOffset(_) => todo!(),
-                ConfigChange::BorderStyle(border_style) => todo!(),
-                ConfigChange::BorderImplementation(border_implementation) => todo!(),
-                ConfigChange::BorderZOrder(zorder) => todo!(),
-                ConfigChange::SingleColor(color) => todo!(),
-                ConfigChange::MonocleColor(color) => todo!(),
-                ConfigChange::UnfocusedColor(color) => todo!(),
-                ConfigChange::FloatingColor(color) => todo!(),
-            },
+            Message::ConfigChange(change) => {
+                let config = border_config_from_static_mut(config);
+                match change {
+                    ConfigChange::Border(enable) => {
+                        *config.border = Some(enable);
+                    }
+                    ConfigChange::BorderWidth(width) => {
+                        *config.border_width = Some(width);
+                    }
+                    ConfigChange::BorderOffset(offset) => {
+                        *config.border_offset = Some(offset);
+                    }
+                    ConfigChange::BorderStyle(border_style) => {
+                        *config.border_style = Some(border_style)
+                    }
+                    ConfigChange::BorderImplementation(border_implementation) => {
+                        *config.border_implementation = Some(border_implementation)
+                    }
+                    ConfigChange::BorderZOrder(zorder) => {
+                        *config.border_z_order = zorder;
+                    }
+                    ConfigChange::SingleColor(color) => {
+                        if let Some(colours) = config.border_colours {
+                            colours.single = color.map(from_color);
+                        } else {
+                            *config.border_colours = Some(BorderColours {
+                                single: color.map(from_color),
+                                stack: None,
+                                monocle: None,
+                                floating: None,
+                                unfocused: None,
+                            });
+                        }
+                        self.show_single_picker = false;
+                    }
+                    ConfigChange::MonocleColor(color) => {
+                        if let Some(colours) = config.border_colours {
+                            colours.monocle = color.map(from_color);
+                        } else {
+                            *config.border_colours = Some(BorderColours {
+                                single: None,
+                                stack: None,
+                                monocle: color.map(from_color),
+                                floating: None,
+                                unfocused: None,
+                            });
+                        }
+                        self.show_monocle_picker = false;
+                    }
+                    ConfigChange::UnfocusedColor(color) => {
+                        if let Some(colours) = config.border_colours {
+                            colours.unfocused = color.map(from_color);
+                        } else {
+                            *config.border_colours = Some(BorderColours {
+                                single: None,
+                                stack: None,
+                                monocle: None,
+                                floating: None,
+                                unfocused: color.map(from_color),
+                            });
+                        }
+                        self.show_unfocused_picker = false;
+                    }
+                    ConfigChange::FloatingColor(color) => {
+                        if let Some(colours) = config.border_colours {
+                            colours.floating = color.map(from_color);
+                        } else {
+                            *config.border_colours = Some(BorderColours {
+                                single: None,
+                                stack: None,
+                                monocle: None,
+                                floating: color.map(from_color),
+                                unfocused: None,
+                            });
+                        }
+                        self.show_floating_picker = false;
+                    }
+                    ConfigChange::StackColor(color) => {
+                        if let Some(colours) = config.border_colours {
+                            colours.stack = color.map(from_color);
+                        } else {
+                            *config.border_colours = Some(BorderColours {
+                                single: None,
+                                stack: color.map(from_color),
+                                monocle: None,
+                                floating: None,
+                                unfocused: None,
+                            });
+                        }
+                        self.show_stack_picker = false;
+                    }
+                }
+            }
             Message::ToggleSinglePicker(show) => {
                 self.show_single_picker = show;
             }
@@ -114,6 +199,9 @@ impl Border {
             }
             Message::ToggleFloatingPicker(show) => {
                 self.show_floating_picker = show;
+            }
+            Message::ToggleStackPicker(show) => {
+                self.show_stack_picker = show;
             }
         }
         (Action::None, Task::none())
@@ -130,9 +218,9 @@ impl Border {
             [
                 opt_helpers::toggle_with_disable_default_no_option(
                     "Enable Border",
-                    Some(""),
-                    *config.border.unwrap_or(&true),
-                    true,
+                    Some("Display an active window border (default: false)"),
+                    *config.border.unwrap_or(&false),
+                    false,
                     |v| Message::ConfigChange(ConfigChange::Border(v)),
                     None,
                 ),
@@ -144,11 +232,23 @@ impl Border {
                     |value| Message::ConfigChange(ConfigChange::BorderWidth(value)),
                     None,
                 ),
+                opt_helpers::number_with_disable_default(
+                    "Border Offset",
+                    Some("Offset of the window border (default: -1)"),
+                    *config.border_offset.unwrap_or(&-1),
+                    -1,
+                    |value| Message::ConfigChange(ConfigChange::BorderOffset(value)),
+                    None,
+                ),
                 opt_helpers::choose_with_disable_default(
                     "Border Style",
-                    Some(""),
+                    Some("Active window border style (default: System)"),
                     vec![],
-                    [BorderStyle::System, BorderStyle::Square, BorderStyle::Rounded],
+                    [
+                        BorderStyle::System,
+                        BorderStyle::Square,
+                        BorderStyle::Rounded,
+                    ],
                     Some(config.border_style.map_or(BorderStyle::System, |bs| *bs)),
                     |selected| {
                         Message::ConfigChange(ConfigChange::BorderStyle(
@@ -158,45 +258,111 @@ impl Border {
                     Some(BorderStyle::System),
                     None,
                 ),
-                // opt_helpers::color(
-                //     "Stackbar Background Color",
-                //     Some("Tab background color. (default: '0x333333')"),
-                //     self.show_background_picker,
-                //     config
-                //         .tabs
-                //         .as_ref()
-                //         .and_then(|t| t.background.map(into_color)),
-                //     Some(iced::color!(0x333333)),
-                //     Message::ToggleSinglePicker,
-                //     |v| Message::ConfigChange(ConfigChange::SingleColor(v)),
-                //     None,
-                // ),
-                // opt_helpers::color(
-                //     "Stackbar Focused Text Color",
-                //     Some("Focused Tab text color. (default: '0xFFFFFF')"),
-                //     self.show_focused_text_picker,
-                //     config
-                //         .tabs
-                //         .as_ref()
-                //         .and_then(|t| t.focused_text.map(into_color)),
-                //     Some(iced::color!(0xffffff)),
-                //     Message::ToggleMonoclePicker,
-                //     |v| Message::ConfigChange(ConfigChange::MonocleColor(v)),
-                //     None,
-                // ),
-                // opt_helpers::color(
-                //     "Stackbar Unfocused Text Color",
-                //     Some("Unfocused Tab text color. (default: '0xB3B3B3')"),
-                //     self.show_unfocused_text_picker,
-                //     config
-                //         .tabs
-                //         .as_ref()
-                //         .and_then(|t| t.unfocused_text.map(into_color)),
-                //     Some(iced::color!(0xb3b3b3)),
-                //     Message::ToggleUnfocusedPicker,
-                //     |v| Message::ConfigChange(ConfigChange::UnfocusedColor(v)),
-                //     None,
-                // ),
+                opt_helpers::choose_with_disable_default(
+                    "Border Implementation",
+                    Some("Active window border implementation (default: Komorebi)"),
+                    vec![
+                        t("Selected: 'Komorebi' -> Use the adjustable komorebi border implementation").into(),
+                        t("Selected: 'Windows' -> Use the thin Windows accent border implementation").into(),
+                    ],
+                    [
+                        BorderImplementation::Komorebi,
+                        BorderImplementation::Windows
+                    ],
+                    Some(config.border_implementation.map_or(BorderImplementation::Komorebi, |bi| *bi)),
+                    |selected| {
+                        Message::ConfigChange(ConfigChange::BorderImplementation(
+                            selected.unwrap_or(BorderImplementation::Komorebi),
+                        ))
+                    },
+                    Some(BorderImplementation::Komorebi),
+                    None,
+                ),
+                opt_helpers::choose_with_disable_default(
+                    "Border Z Order",
+                    Some("Active window border z-order (default: None (uses System value))"),
+                    vec![],
+                    [
+                        DisplayOption(None),
+                        DisplayOption(Some(ZOrder::Top)),
+                        DisplayOption(Some(ZOrder::Bottom)),
+                        DisplayOption(Some(ZOrder::TopMost)),
+                        DisplayOption(Some(ZOrder::NoTopMost)),
+                    ],
+                    Some(config.border_z_order.map_or(DisplayOption(None), |bi| DisplayOption(Some(*bi)))),
+                    |selected| {
+                        Message::ConfigChange(ConfigChange::BorderZOrder(
+                            selected.and_then(|s| s.0),
+                        ))
+                    },
+                    Some(DisplayOption(None)),
+                    None,
+                ),
+                opt_helpers::color(
+                    "Focused Window Border Color",
+                    Some("Border colour when the container contains a single window and is focused"),
+                    self.show_single_picker,
+                    config
+                        .border_colours
+                        .as_ref()
+                        .and_then(|bc| bc.single.map(into_color)),
+                    Some(iced::color!(66, 165, 245)),
+                    Message::ToggleSinglePicker,
+                    |v| Message::ConfigChange(ConfigChange::SingleColor(v)),
+                    None,
+                ),
+                opt_helpers::color(
+                    "Unfocused Window Border Color",
+                    Some("Border colour when the container is unfocused"),
+                    self.show_unfocused_picker,
+                    config
+                        .border_colours
+                        .as_ref()
+                        .and_then(|bc| bc.unfocused.map(into_color)),
+                    Some(iced::color!(128, 128, 128)),
+                    Message::ToggleUnfocusedPicker,
+                    |v| Message::ConfigChange(ConfigChange::UnfocusedColor(v)),
+                    None,
+                ),
+                opt_helpers::color(
+                    "Monocle Window Border Color",
+                    Some("Border colour when the container is in monocle mode"),
+                    self.show_monocle_picker,
+                    config
+                        .border_colours
+                        .as_ref()
+                        .and_then(|bc| bc.monocle.map(into_color)),
+                    Some(iced::color!(255, 51, 153)),
+                    Message::ToggleMonoclePicker,
+                    |v| Message::ConfigChange(ConfigChange::MonocleColor(v)),
+                    None,
+                ),
+                opt_helpers::color(
+                    "Stack Window Border Color",
+                    Some("Border colour when the container contains multiple windows and is focused"),
+                    self.show_stack_picker,
+                    config
+                        .border_colours
+                        .as_ref()
+                        .and_then(|bc| bc.stack.map(into_color)),
+                    Some(iced::color!(0, 165, 66)),
+                    Message::ToggleStackPicker,
+                    |v| Message::ConfigChange(ConfigChange::StackColor(v)),
+                    None,
+                ),
+                opt_helpers::color(
+                    "Floating Window Border Color",
+                    Some("Border colour when the container is in floating mode and focused"),
+                    self.show_floating_picker,
+                    config
+                        .border_colours
+                        .as_ref()
+                        .and_then(|bc| bc.floating.map(into_color)),
+                    Some(iced::color!(245, 245, 165)),
+                    Message::ToggleFloatingPicker,
+                    |v| Message::ConfigChange(ConfigChange::FloatingColor(v)),
+                    None,
+                ),
             ],
         )
     }
@@ -216,13 +382,13 @@ fn border_config_from_static(config: &StaticConfig) -> BorderConfig {
 
 fn border_config_from_static_mut(config: &mut StaticConfig) -> BorderConfigMut {
     BorderConfigMut {
-        border: config.border.as_mut(),
-        border_colours: config.border_colours.as_mut(),
-        border_implementation: config.border_implementation.as_mut(),
-        border_style: config.border_style.as_mut(),
-        border_width: config.border_width.as_mut(),
-        border_offset: config.border_offset.as_mut(),
-        border_z_order: config.border_z_order.as_mut(),
+        border: &mut config.border,
+        border_colours: &mut config.border_colours,
+        border_implementation: &mut config.border_implementation,
+        border_style: &mut config.border_style,
+        border_width: &mut config.border_width,
+        border_offset: &mut config.border_offset,
+        border_z_order: &mut config.border_z_order,
     }
 }
 
