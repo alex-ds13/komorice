9eda72c0cd889635c895ebe7de5db566370482ad
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2025-02-19 23:55:11 +0000

    feat(apperror): add `AppError` view function

--- src/apperror.rs
+++ src/apperror.rs
@@ -1,3 +1,11 @@
+use crate::widget::{self, icons};
+use crate::Message;
+
+use iced::{
+    widget::{column, container, row, text, Text},
+    Border, Center, Color, Element, Fill,
+};
+
 #[derive(Debug, Clone)]
 pub struct AppError {
     pub title: String,
@@ -10,4 +18,72 @@ pub enum AppErrorKind {
     Info,
     Warning,
     Error,
+}
+
+impl AppError {
+    pub fn view(&self) -> Element<Message> {
+        column![row![self.kind.view(), text(&self.title).size(18)]
+            .spacing(10)
+            .align_y(Center)]
+        .push_maybe(self.description.as_ref().map(|d| {
+            container(text(d))
+                .width(Fill)
+                .style(|_| container::Style {
+                    background: Some(
+                        Color {
+                            a: 0.5,
+                            ..Color::BLACK
+                        }
+                        .into(),
+                    ),
+                    border: Border {
+                        color: self.kind.color(),
+                        width: 1.0,
+                        radius: 0.0.into(),
+                    },
+                    ..container::Style::default()
+                })
+                .padding(10.0)
+        }))
+        .spacing(10)
+        .into()
+    }
+}
+
+impl AppErrorKind {
+    fn color(&self) -> Color {
+        match self {
+            AppErrorKind::Info => *widget::BLUE,
+            AppErrorKind::Warning => *widget::YELLOW,
+            AppErrorKind::Error => *widget::RED,
+        }
+    }
+
+    fn icon(&self) -> Text {
+        match self {
+            AppErrorKind::Info => icons::info(),
+            AppErrorKind::Warning => icons::warning(),
+            AppErrorKind::Error => icons::error(),
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        container(self.icon().size(20).color(Color {
+            a: 0.9,
+            ..Color::BLACK
+        }))
+        .style(move |t| container::Style {
+            background: Some(self.color().into()),
+            border: Border {
+                color: Color::TRANSPARENT,
+                width: 0.0,
+                radius: 13.0.into(),
+            },
+            ..container::transparent(t)
+        })
+        .clip(false)
+        .center(26)
+        .padding(0.0)
+        .into()
+    }
 }
