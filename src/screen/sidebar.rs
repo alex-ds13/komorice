f1c54e1828e7e18ee0355636ca7796b1056e96d2
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2025-04-07 18:15:54 +0100

    refactor(sidebar): simplify sidebar to one screen
    - Simplifies the sidebar to one screen only that uses a new field called
      `config_type` to define which set of buttons to show. This is done by
      the new function `.get_screens()`. The home button is now created
      directly and can't be select so it is just a simple button (still
      needs to be inside a container so that the fixed_width trick works).
    - The sidebar now also has a different field for each config type
      selection, the `komorebi_selected` and `whkd_selected`. This allows us
      to keep track of the selected screen even if we go to the home screen
      to quickly go change something on another config type and come back.
    - For the previous to work properly we also added a new message called
      `Message::SetHomeScreen` which is the one sent by the home button, so
      that we don't change the selected screen when pressing the home button
      but simply send the new `Action::SetHomeScreen` to the parent.
    - A new public function was created for the sidebar called
      `selected_screen()` which lets us get the currently selected screen
      according to the current config type. This is used on a Home action
      that changes the config type, so that we can change the config type of
      the sidebar and then get the selected screen for that config type and
      set our `main_screen` to that one.
    - The `ConfigType` definition was moved to `screen.rs`.
    - The Home buttons now don't try to set a screen, they simply change the
      config type and we get the screen to show from the sidebar selection.
    - Removed the `whkd_sidebar.rs` file.
    - Removed the `Sidebar` enum from `screen.rs`.

--- src/screen/sidebar.rs
+++ src/screen/sidebar.rs
@@ -1,4 +1,4 @@
-use super::{Screen, SidebarAction, SidebarMessage};
+use super::{ConfigType, Screen};
 
 use crate::SCREENS_TO_RESET;
 
@@ -10,62 +10,76 @@ use iced::{
     Task,
 };
 
+#[derive(Clone, Debug)]
+pub enum Message {
+    SetHomeScreen,
+    SelectScreen(Screen),
+}
+
+#[derive(Clone, Debug)]
+pub enum Action {
+    None,
+    SetHomeScreen,
+    UpdateMainScreen(Screen),
+}
+
 #[derive(Debug, Clone)]
 pub struct Sidebar {
-    pub selected_screen: Screen,
+    pub komorebi_selected: Screen,
+    pub whkd_selected: Screen,
+    pub config_type: ConfigType,
 }
 
 impl Default for Sidebar {
     fn default() -> Self {
         Self {
-            selected_screen: Screen::General,
+            komorebi_selected: Screen::General,
+            whkd_selected: Screen::Whkd,
+            config_type: ConfigType::Komorebi,
         }
     }
 }
 
 impl Sidebar {
-    pub fn update(&mut self, message: SidebarMessage) -> (SidebarAction, Task<SidebarMessage>) {
+    pub fn update(&mut self, message: Message) -> (Action, Task<Message>) {
         match message {
-            SidebarMessage::SelectScreen(screen) => {
-                if screen != self.selected_screen || SCREENS_TO_RESET.contains(&screen) {
-                    self.selected_screen = screen.clone();
-                    return (SidebarAction::UpdateMainScreen(screen), Task::none());
-                }
+            Message::SetHomeScreen => {
+                return (Action::SetHomeScreen, Task::none());
             }
+            Message::SelectScreen(screen) => match self.config_type {
+                ConfigType::Komorebi => {
+                    if screen != self.komorebi_selected || SCREENS_TO_RESET.contains(&screen) {
+                        self.komorebi_selected = screen.clone();
+                        return (Action::UpdateMainScreen(screen), Task::none());
+                    }
+                }
+                ConfigType::Whkd => {
+                    if screen != self.whkd_selected || SCREENS_TO_RESET.contains(&screen) {
+                        self.whkd_selected = screen.clone();
+                        return (Action::UpdateMainScreen(screen), Task::none());
+                    }
+                }
+            },
         }
-        (SidebarAction::None, Task::none())
+        (Action::None, Task::none())
     }
 
-    pub fn view(&self) -> Element<SidebarMessage> {
-        let home = screen_button(Screen::Home, &self.selected_screen);
-        let general = screen_button(Screen::General, &self.selected_screen);
-        let monitors = screen_button(Screen::Monitors, &self.selected_screen);
-        let border = screen_button(Screen::Border, &self.selected_screen);
-        let stackbar = screen_button(Screen::Stackbar, &self.selected_screen);
-        let transparency = screen_button(Screen::Transparency, &self.selected_screen);
-        let animation = screen_button(Screen::Animations, &self.selected_screen);
-        let theme = screen_button(Screen::Theme, &self.selected_screen);
-        let rules = screen_button(Screen::Rules, &self.selected_screen);
-        let debug = screen_button(Screen::LiveDebug, &self.selected_screen);
-        let settings = screen_button(Screen::Settings, &self.selected_screen);
+    pub fn view(&self) -> Element<Message> {
+        let home = container(
+            button(Screen::Home)
+                .on_press(Message::SetHomeScreen)
+                .style(button::secondary)
+                .width(Fill),
+        )
+        .width(Fill);
+        let screen_buttons = self.get_screens();
         let fixed_width = Space::new(120, Shrink);
         let main_content = scrollable(
-            column![
-                fixed_width,
-                general,
-                monitors,
-                border,
-                stackbar,
-                transparency,
-                animation,
-                theme,
-                rules,
-                debug,
-                settings,
-            ]
-            .spacing(10)
-            .padding(padding::right(15).left(5).bottom(0))
-            .width(Shrink),
+            column![fixed_width]
+                .extend(screen_buttons)
+                .spacing(10)
+                .padding(padding::right(15).left(5).bottom(0))
+                .width(Shrink),
         )
         .height(Fill);
         let fixed_width_wider = Space::new(135, Shrink);
@@ -80,13 +94,44 @@ impl Sidebar {
 
         column![main_content, bottom_content].into()
     }
+
+    pub fn selected_screen(&self) -> Screen {
+        match self.config_type {
+            ConfigType::Komorebi => self.komorebi_selected.clone(),
+            ConfigType::Whkd => self.whkd_selected.clone(),
+        }
+    }
+
+    fn get_screens(&self) -> Vec<Element<Message>> {
+        match self.config_type {
+            ConfigType::Komorebi => [
+                Screen::General,
+                Screen::Monitors,
+                Screen::Border,
+                Screen::Stackbar,
+                Screen::Transparency,
+                Screen::Animations,
+                Screen::Theme,
+                Screen::Rules,
+                Screen::LiveDebug,
+                Screen::Settings,
+            ]
+            .into_iter()
+            .map(|s| screen_button(s, &self.komorebi_selected).into())
+            .collect(),
+            ConfigType::Whkd => [Screen::Whkd, Screen::Settings]
+                .into_iter()
+                .map(|s| screen_button(s, &self.whkd_selected).into())
+                .collect(),
+        }
+    }
 }
 
-fn screen_button(screen: Screen, selected: &Screen) -> Container<SidebarMessage> {
+fn screen_button(screen: Screen, selected: &Screen) -> Container<Message> {
     let is_selected = &screen == selected;
     container(
         button(&screen)
-            .on_press(SidebarMessage::SelectScreen(screen))
+            .on_press(Message::SelectScreen(screen))
             .style(move |t, s| {
                 if is_selected {
                     button::primary(t, s)
